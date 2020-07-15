1793c332b233404664ee223b59706097cd57bbaf
Anton <akarnomazov@gmail.com>
2020-07-15 19:42:28 +0300

    add building in editor menu

---
+++ GO_Editor/Assets/Scripts/LevelEditor/BuildingPlacement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class BuildingPlacement : MonoBehaviour
+{
+    [SerializeField] private GameObject floorPref;
+
+    private ObjectSelector selector;
+    private IDisposable routine;
+
+    private void Awake()
+    {
+        selector = GetComponent<ObjectSelector>();
+
+        LevelInitializer.StartAddObjEvent.AddListener(() =>
+        {
+            if (routine != null)
+                routine.Dispose();
+        });
+    }
+
+    public void AddHightFloor()
+    {
+        LevelInitializer.StartAddObjEvent.Invoke();
+
+        routine = Observable
+            .FromCoroutine(() => selector.SelectNodeRoutine("Select node to place object"))
+            .Subscribe(_ =>
+            {
+                selector.Reset();
+                LevelInitializer.EndAddObjEvent?.Invoke();
+            });
+    }
+
+    private GameObject PlaceFloor(GameObject point)
+    {
+        var instance = Instantiate(floorPref, point.transform.position, Quaternion.identity);
+        return instance;
+    }
+}
